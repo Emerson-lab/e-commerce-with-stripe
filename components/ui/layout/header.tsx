9b23b193f5627ec93e5725a2a79401af0c3f5d96
Emerson Trindade <trinddade29@gmail.com>
2023-10-07 19:07:00 -0300

    chore: config app

---
+++ components/ui/layout/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header className="stick top-0 z-10 shadow">
+      <div className="container ms-auto p-4 flex items-center justify-between">
+        
+      </div>
+    </header>
+  )
+}
